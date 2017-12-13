f4510ddd97bc4628d4dc79124e3e186f99a39694
Mohammed Saad <34467824+Saad789@users.noreply.github.com>
2017-12-13 13:05:13 +0530

    Add files via upload

---
+++ Beginner Java/src/JavaTutorial/DivisionProgram.java
@@ -0,0 +1,20 @@
+package JavaTutorial;
+
+public class DivisionProgram {
+
+	public static void main(String[] args) {
+		int number1 = 40;
+		int number2 = 2;
+		double div = number1 / number2;
+		
+		if(div>0){
+		System.out.println("the division of two numbers is possible"+ ' '+div);
+		
+		}
+		else{
+			System.out.println("the division is not possible");
+		}
+
+	}
+
+}
