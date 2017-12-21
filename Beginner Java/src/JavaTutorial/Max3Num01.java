0ae2a52ae5a1f5fcfacbc18c40b1a2d5497004af
Mohammed Saad <34467824+Saad789@users.noreply.github.com>
2017-12-22 00:29:22 +0530

    Update Max3Num01.java

--- Beginner Java/src/JavaTutorial/Max3Num01.java
+++ Beginner Java/src/JavaTutorial/Max3Num01.java
@@ -4,27 +4,28 @@ public class Max3Num01 {
 	public static void main(String[] args) {
 		int num1 = 30;
 		int num2 = 20;
-	    int num3 = 10;
+	        int num3 = 10;
 	    System.out.println(getMaxNumber(num1,num2,num3));
-        }
+         }
 	
 	public static int getMaxNumber(int num1,int num2,int num3){
 		
-		    if(num1>0 && num2>0 && num3>0){
+		     if(num1>0 && num2>0 && num3>0){
 		     if (num1>num2 && num1>num3)
 		    {
 			
-				return num1;
+		      return num1;
+		     }
+		       else if(num2>num3){
+			return num2;
+		     }
+		    
+			else{
+			 return num3;
 			}
-			else if(num2>num3){
-				return num2;
-			}
-		    else{
-				return num3;
-			}
-	    }
-	        else{ 
-		        return -1;
+	          }
+	                else{ 
+		         return -1;
 	        }
           }
 }
